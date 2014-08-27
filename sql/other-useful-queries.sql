3e549b203722eca14b55cdc126e7d3e29efa22a0
Chris Henrick <chrishenrick@gmail.com>
2014-08-27 17:22:11 -0400

    added other useful sql queries

--- sql/other-useful-queries.sql
+++ sql/other-useful-queries.sql
@@ -1,27 +1,3 @@
--------- SQL used in the tutorial --------
-
---- select population density by county
- SELECT pop_sqkm, 
-     round(
-         pop / 
-         (ST_Area(the_geom::geography)/1000000)) 
-     as psqkm 
-     FROM us_counties
-
---- select the number of tornados by county
---- solution based on answer from: http://gis.stackexchange.com/questions/54661/count-points-in-polygon-with-postgis
-SELECT us_counties.cartodb_id, count(tornados.the_geom) AS total 
-FROM us_counties LEFT JOIN tornados 
-ON st_contains(us_counties.the_geom,tornados.the_geom) 
-GROUP BY us_counties.cartodb_id
-
---- easier way:
-UPDATE us_countries SET tornados_by_county = 
-    (
-        SELECT count(1) FROM tornados 
-        WHERE st_contains(us_counties.the_geom, tornados.the_geom) 
-    )
-
 -------- Other Useful SQL Statements --------
 --- these were not used in the tutorial but feel free to try them out!
 
@@ -51,5 +27,3 @@ DELETE FROM table_name
 
 --- change the data column from string to int:
 ALTER TABLE table_name ALTER COLUMN column_name TYPE INT USING column_name::int
-
-
